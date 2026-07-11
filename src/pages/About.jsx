8541bc8e5c5a43fb400d32b7aeb8b7391f5f4658
Mohd Aadil <119642147+Aadil1720@users.noreply.github.com>
2026-07-11 23:27:08 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
           </div>
           <p className="featured-name">
             <ReactTypingEffect
-              text={['Data Engineer', 'Developer', 'Quick Learner']}
+              text={['Data Engineer', 'Databricks Certified', 'Java Developer']}
               speed={200}
               eraseSpeed={100}
               eraseDelay={1500}
